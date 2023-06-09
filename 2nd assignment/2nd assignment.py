10fd29e86e35028d7dcfe9d550de72c2780114f9
Mohamed Sathik <satikofficial2028@gmail.com>
2023-06-09 14:17:01 +0530

    2nd assignment

---
+++ 2nd assignment/2nd assignment.py
@@ -0,0 +1,18 @@
+import json
+
+
+def second_assignment():
+
+    with open("2nd assignment\ex5.json","r") as f:
+        ex5=json.load(f)
+
+        for x in ex5:
+            if x['name'] == 'Old Fashioned':
+                z=x['batters']['batter']
+
+                z.append({'id': '1003', 'type': 'Coffee'})
+
+    with open("2nd assignment\ex5.json","w") as out:
+        json.dump(ex5,out,indent=4)
+
+second_assignment()
